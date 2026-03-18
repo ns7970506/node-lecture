bed6b270ebdbbb4d39d573a289d32e5b8f3d2caf
Nikhil <ns7970506@gmail.com>
2026-03-18 15:32:04 +0530

    first commit

--- Lec-3 first-node-server/app.js
+++ Lec-3 first-node-server/app.js
@@ -5,6 +5,6 @@ const server = http.createServer( (req,res)=>{
 })
 const PORT = 3001
 server.listen(PORT,()=>{
-   console.log('Server running at http://localhost:${PORT}');
+   console.log(`Server running at http://localhost:${PORT}`);
     
 })
